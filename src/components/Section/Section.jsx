f2428b2f5ae3cb17e016e5e35de60de2ed73d7ed
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-28 10:52:06 +0200

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { Box } from 'components/Box';
 import { Title } from './Section.styled';
 
@@ -9,7 +9,7 @@ export const Section = ({ title, children = [] }) => (
   </Box>
 );
 
-Section.propTypes = {
-  title: PropTypes.string.isRequired,
-  children: PropTypes.node,
-};
+// Section.propTypes = {
+//   title: PropTypes.string.isRequired,
+//   children: PropTypes.node,
+// };
